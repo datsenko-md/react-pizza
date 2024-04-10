c46a2db3934a372c33b94d326b144f679b1e5214
Max Datsenko <datsenko.md@gmail.com>
2024-04-10 19:59:24 +0300

    add categories change

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import cn from 'classnames';
 
 function Categories() {
+  const categories = [
+    { id: 1, name: 'Все' },
+    { id: 2, name: 'Овощные' },
+    { id: 3, name: 'Вегетарианские' },
+    { id: 4, name: 'Гриль' },
+    { id: 5, name: 'Острые' },
+    { id: 6, name: 'Закрытые' },
+  ];
+
+  const [active, setActive] = React.useState(categories[0].id);
+
+  const renderItem = ({ id, name }) => (
+    <li
+      key={id}
+      className={cn({ active: id === active })}
+      onClick={() => setActive(id)}
+    >
+      {name}
+    </li>
+  );
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map(renderItem)}
       </ul>
     </div>
   );

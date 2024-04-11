7dfb5644443789e110a77cd197b3bb67a03fdd6f
Max Datsenko <datsenko.md@gmail.com>
2024-04-11 17:11:40 +0300

    set data in pizza block

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,29 +1,44 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-/* eslint-disable react/prop-types */
-
 import React from 'react';
+import cn from 'classnames';
 
-function PizzaBlock(props) {
-  const { title, price } = props;
+function PizzaBlock({
+  title, price, imageUrl, types, sizes,
+}) {
+  const typesNames = ['тонкое', 'традиционное'];
   const [count, setCount] = React.useState(0);
+  const [activeType, setActiveType] = React.useState(types[0]);
+  const [activeSize, setActiveSize] = React.useState(sizes[0]);
 
   return (
     <div className="pizza-block">
       <img
         className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+        src={imageUrl}
         alt="Pizza"
       />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((type) => (
+            <li
+              key={type}
+              onClick={() => setActiveType(type)}
+              className={cn({ active: type === activeType })}
+            >
+              {typesNames[type]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size) => (
+            <li
+              key={size}
+              onClick={() => setActiveSize(size)}
+              className={cn({ active: size === activeSize })}
+            >
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
